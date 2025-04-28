7f2a50237e44606912c7f86ab4366ad4c4f3b160
fagathe-dev <agathefrederick@gmail.com>
2025-04-28 06:52:45 +0200

    ui(#14): mise en place de la template des pages authentifications

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthController extends AbstractController
+{
+
+
+    public function __construct() {}
+
+    #[Route('/auth', name: 'auth')]
+    public function index(): Response
+    {
+        return $this->render('auth/index.html.twig');
+    }
+}
