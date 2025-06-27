435fefc0b8b9195b74472479df0d87b6e7aa244b
fagathe-dev <agathefrederick@gmail.com>
2025-06-27 12:24:15 +0200

    feat(#158): architecture des fichiers

---
+++ admin/src/AdminBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Admin;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class AdminBundle extends Bundle
+{
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
+}
